5576c7a87d48eda6e3ece89f9f822701efae0081
arshil <arshilsingh711@gmail.com>
2024-07-21 12:37:49 +0530

    created package setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "financebot",
+    version="0.0.0",
+    author= "Arshil",
+    author_email="arshilsingh711@gmail.com",
+    packages= find_packages(),
+    install_requires = [],
+)
